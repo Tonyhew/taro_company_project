6e091954e5eb9a4187e667ab6d20668ecd401330
tonyhew <916390127@qq.com>
2020-12-12 17:25:37 +0800

    图片路径修改

--- src/app.config.js
+++ src/app.config.js
@@ -21,27 +21,27 @@ export default {
         pagePath: "pages/index/index",
         text: "首页",
         // 未点击时显示的图片
-        iconPath: "images/icon/theme3_63.png",
+        iconPath: "Images/icon/theme3_63.png",
         // 点击后显示的图片
-        selectedIconPath: "images/icon/theme3_64.png"
+        selectedIconPath: "Images/icon/theme3_64.png"
       },
       {
         pagePath: "pages/project/project",
         text: "项目",
-        iconPath: "images/icon/theme3_67.png",
-        selectedIconPath: "images/icon/theme3_68.png"
+        iconPath: "Images/icon/theme3_67.png",
+        selectedIconPath: "Images/icon/theme3_68.png"
       },
       {
         pagePath: "pages/scores/scores",
         text: "积分",
-        iconPath: "images/icon/theme3_61.png",
-        selectedIconPath: "images/icon/theme3_62.png"
+        iconPath: "Images/icon/theme3_61.png",
+        selectedIconPath: "Images/icon/theme3_62.png"
       },
       {
         pagePath: "pages/mine/mine",
         text: "我的",
-        iconPath: "images/icon/theme3_65.png",
-        selectedIconPath: "images/icon/theme3_66.png"
+        iconPath: "Images/icon/theme3_65.png",
+        selectedIconPath: "Images/icon/theme3_66.png"
       },
     ]
   }
