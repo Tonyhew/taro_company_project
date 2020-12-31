166006c5fa4a4b6975de996b4c46d8afe57a2085
tonyhew <916390127@qq.com>
2020-12-31 14:41:30 +0800

    首页组件-亦医亦景

---
+++ src/components/index/Environment.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react'
+import { View, Swiper, SwiperItem, Image } from '@tarojs/components'
+import classNames from 'classnames'
+
+class Environment extends Component {
+
+  state = {
+    currentIndex: 0
+  }
+
+  componentDidShow() {
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      currentIndex: e.detail.current
+    })
+  }
+
+  render() {
+    return (
+      <View class="environmentalS">
+        <View class="name" style="margin-left:16rpx;margin-bottom:0rpx">
+          <View>{this.props.environmentTitle.homeModuleName}</View>
+          <View>{this.props.environmentTitle.subtitle != null ? this.props.environmentTitle.subtitle : ''}</View>
+        </View>
+        <Swiper autoplay interval={5000} circular previous-margin="80rpx" next-margin="80rpx" onChange={this.handleChange}>
+          {
+            this.props.environmental.map((envir, index) => {
+              return (
+                <SwiperItem key={index}>
+                  <Image src={envir.pictureUrl} class={classNames('slide-image', this.state.currentIndex == index ? 'active' : '')} />
+                </SwiperItem>
+              )
+            })
+          }
+        </Swiper>
+      </View>
+    )
+  }
+}
+
+export default Environment
+
+
+
+
+
+
+
+
+
+
+
+
+
+
