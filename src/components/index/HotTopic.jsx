63f575b221c7db41409c0118fbbdd8a8a525e9fa
tonyhew <916390127@qq.com>
2020-12-31 14:55:31 +0800

    首页组件-热门话题

---
+++ src/components/index/HotTopic.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { View, Image, Navigator } from '@tarojs/components'
+import icon1 from '../../Images/icon/icon-1.png'
+
+class HotTopic extends Component {
+
+  componentDidShow() {
+    console.log(this.props)
+  }
+
+  render() {
+    return (
+      <View class="topic">
+        <View class="name">
+          <View>{this.props.hotTopicTitle.homeModuleName}</View>
+          <View>{this.props.hotTopicTitle.subtitle != null ? this.props.hotTopicTitle.subtitle : ''}</View>
+        </View>
+        <Navigator url="/pages/topic/topic?&currentTab=0">
+          <View class="more">更多 》</View>
+        </Navigator>
+        {
+          this.props.hotTopicList.map(item => {
+            return (
+              <Navigator key={item.id} class="topicList" url={item.path != null ? item.path : '/pages/topic/topic?&currentTab=0'}>
+                <Image src={icon1}></Image>
+                <View class="topicTitle">{item.topicTypeName}</View>
+              </Navigator>
+            )
+          })
+        }
+      </View>
+    )
+  }
+}
+
+export default HotTopic
+
+
+
+
+
+
+
+
