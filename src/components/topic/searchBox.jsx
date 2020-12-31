851eb85d1a22bddf1b2706d9d2c3db60d1e48c44
tonyhew <916390127@qq.com>
2020-12-31 14:58:59 +0800

    话题页面（未完成）

---
+++ src/components/topic/searchBox.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import { View } from '@tarojs/components'
+
+
+
+class SearchBox extends Component {
+
+  render() {
+
+    return (
+      <View>
+        SearchBox
+      </View>
+    )
+
+  }
+
+}
+
+
+export default SearchBox
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
