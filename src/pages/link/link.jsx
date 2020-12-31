cd3063d5ace7efa4e8901690c23f71bcc9ed795a
tonyhew <916390127@qq.com>
2020-12-31 15:00:49 +0800

    小程序页面

---
+++ src/pages/link/link.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { View } from '@tarojs/components';
+
+class Link extends Component {
+
+  componentDidShow(o) {
+  }
+
+  render() {
+    return (
+      <View>
+
+      </View>
+    )
+  }
+
+}
+
+export default Link
+
+
+
+
+
---
+++ src/pages/topic/topic.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Taro from '@tarojs/taro';
+import { View } from '@tarojs/components'
+import './topic.less'
+
+class Topic extends Component {
+
+  render() {
+    return (
+      <View>
+        
+      </View>
+    )
+  }
+
+}
+
+
+export default Topic
+
+
+
+
+
+
+
+
+
+
+
+
