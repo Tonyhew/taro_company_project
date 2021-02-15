58741ef2f14f08243b2cf38e6430e4c5587ae942
tonyhew <916390127@qq.com>
2021-02-15 16:04:38 +0800

    首页Banner组件

--- src/components/index/Banner.jsx
+++ src/components/index/Banner.jsx
@@ -1,8 +1,34 @@
-import React, { Component } from 'react'
-import { Swiper, SwiperItem, Image } from '@tarojs/components'
+import React, { Component } from 'react';
+import Taro from '@tarojs/taro';
+import { Swiper, SwiperItem, Image } from '@tarojs/components';
 
 class Banner extends Component {
-  
+
+  link = (a) => {
+    let t = a.currentTarget.dataset.link,
+      n = a.currentTarget.dataset.appid,
+      e = a.currentTarget.dataset.linkType
+    "" != n && null != n ? Taro.navigateToMiniProgram({
+      appId: n,
+      path: "",
+      success: function (a) {
+      },
+      fail: function () {
+        Taro.showModal({
+          title: "错误",
+          content: "跳转失败"
+        });
+      }
+    }) : "" != t && null != t && (-1 != t.indexOf("../") ? Taro.navigateTo({
+      url: t
+    }) : (t = escape(t), Taro.navigateTo({
+      url: "../link/link?&id=" + t,
+
+    }
+    )));
+
+  }
+
   render() {
     return (
       <Swiper
@@ -19,7 +45,7 @@ class Banner extends Component {
           this.props.banner.map((banner) => {
             return (
               <SwiperItem class="banner_item" key={banner.id}>
-                <Image src={banner.bimg} bindtap={banner.link} mode="aspectFit" class="slide-image"></Image>
+                <Image src={banner.bimg} onClick={this.link.bind(this)} data-link={banner.link} data-linkType='1' mode="aspectFit" class="slide-image"></Image>
               </SwiperItem>
             )
           })
