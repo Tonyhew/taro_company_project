910c358fad46c4def0542f36cb757b4f5641e562
tonyhew <916390127@qq.com>
2021-02-15 16:44:48 +0800

    个人页面骨架屏

---
+++ src/pages/mine/mine_skeleton.jsx
@@ -0,0 +1,147 @@
+import React, { Component } from 'react';
+import { View, Image, Navigator, Text, Button } from '@tarojs/components';
+import './mine_skeleton.less'
+
+class MineSkeleton extends Component {
+
+
+  render() {
+
+    return (
+      <View class="sk-container">
+        <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_462" style="true">
+          <View class="top sk-Image" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_359" style="true">
+            <Image class="seouleaguer sk-Image" id="_n_358" mode="aspectFit" style="true"></Image>
+          </View>
+          <View class="blueCard" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_378" style="true">
+            <Navigator class="setUp" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_361" open-type="navigate" path="true" style="true" version="version">
+              <Image id="_n_360" mode="scaleToFill" style="true" class="sk-Image"></Image>
+              <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_465" style="true" class="sk-transparent sk-Text-14-2857-156 sk-Text">Blue Card</View>
+            </Navigator>
+            <View class="top_View" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_370" style="true">
+              <Image class="avatar sk-Image" id="_n_362" mode="scaleToFill" style="border: none;"></Image>
+              <View class="nick" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_369" style="true">
+                <View class="nickName" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_471" style="true">
+                  <View class="names sk-transparent sk-Text-14-2857-313 sk-Text" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_467" style="color: rgb(255, 255, 255); background-position-x: 50%;">Tony</View>
+                  <View class="grade" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_470" style="true">
+                    <Text id="_n_469" space="true" style="background-color: rgb(222, 28, 42); padding: 0px 2px; background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-961 sk-Text">Lv.number1</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+            <View class="autograph" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_371" style="true"></View>
+            <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_377" style="true">
+              <View class="jifen" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_376" style="true">
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_373" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-354 sk-Text">46</View>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_375" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-547 sk-Text">盐值积分</View>
+              </View>
+            </View>
+          </View>
+          <View class="shuju" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_406" style="true">
+            <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_384" style="flex: 1;Text-align: center;font-size: 3vw;">
+              <View class="Button2" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_381" style="true">
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_380" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-328 sk-Text">0</View>
+              </View>
+              <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_383" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-412 sk-Text">关注数</View>
+            </View>
+            <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_385" style="width: 1px;height: 33px;background-color: #E2E2E2;"></View>
+            <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_391" style="flex: 1;Text-align: center;font-size: 3vw;">
+              <View class="Button2" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_388" style="true">
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_387" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-341 sk-Text">0</View>
+              </View>
+              <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_390" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-43 sk-Text">我喜欢</View>
+            </View>
+            <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_392" style="width: 1px;height: 33px;background-color: #E2E2E2;"></View>
+            <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_398" style="flex: 1;Text-align: center;font-size: 3vw;">
+              <View class="Button2" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_395" style="true">
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_394" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-827 sk-Text">0</View>
+              </View>
+              <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_397" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-712 sk-Text">我收藏</View>
+            </View>
+            <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_399" style="width: 1px;height: 33px;background-color: #E2E2E2;"></View>
+            <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_405" style="flex: 1;Text-align: center;font-size: 3vw;">
+              <View class="Button2" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_402" style="true">
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_401" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-105 sk-Text">0</View>
+              </View>
+              <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_404" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-927 sk-Text">我参与</View>
+            </View>
+          </View>
+          <View class="list" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_424" style="true">
+            <View class="list_content" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_423" style="true">
+              <Navigator class="item" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_410" open-type="navigate" path="true" style="true" version="version">
+                <Image id="_n_407" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_409" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-623 sk-Text">待付款</View>
+              </Navigator>
+              <Navigator class="item" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_414" open-type="navigate" path="true" style="true" version="version">
+                <Image id="_n_411" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_413" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-470 sk-Text">已付款</View>
+              </Navigator>
+              <Navigator class="item" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_418" open-type="navigate" path="true" style="true" version="version">
+                <Image id="_n_415" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_417" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-881 sk-Text">待评价</View>
+              </Navigator>
+              <Navigator class="item" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_422" open-type="navigate" path="true" style="true" version="version">
+                <Image id="_n_419" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_421" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-896 sk-Text">退售后</View>
+              </Navigator>
+            </View>
+          </View>
+          <View class="list2" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_461" style="true">
+            <View class="list_content" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_443" style="true">
+              <Navigator class="item" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_428" open-type="navigate" path="true" style="true" version="version">
+                <Image id="_n_425" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_427" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-323 sk-Text">盐值钱包</View>
+              </Navigator>
+              <Navigator class="item" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_432" open-type="navigate" path="true" style="true" version="version">
+                <Image id="_n_429" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_431" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-580 sk-Text">盐值积分</View>
+              </Navigator>
+              <Navigator class="item" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_436" open-type="navigate" path="true" style="true" version="version">
+                <Image id="_n_433" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_435" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-598 sk-Text">盐值卡券</View>
+              </Navigator>
+              <View class="item" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_442" style="true">
+                <Button app-parameter="true" class="customer_service sk-Button sk-pseudo sk-pseudo-circle" form-type="true" hover-class="Button-hover" hover-start-time="20" hover-stay-time="70" id="_n_438" lang="true" open-type="contact" send-message-img="true"
+                  send-message-path="true" send-message-title="true" session-from="weapp" size="default" style="true" type="true">
+                  <Image id="_n_437" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                </Button>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_439" style="height: 5px;"></View>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_441" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-203 sk-Text">客服中心</View>
+              </View>
+            </View>
+            <View class="list_content" hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_460" style="true">
+              <Navigator class="item" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_447" open-type="navigate" path="true" style="true" version="version">
+                <Image id="_n_444" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_446" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-502 sk-Text">地址管理</View>
+              </Navigator>
+              <Navigator class="item" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_451" open-type="navigate" path="true" style="true" version="version">
+                <Image id="_n_448" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_450" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-28 sk-Text">服务评价</View>
+              </Navigator>
+              <Navigator class="item" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_455" open-type="navigate" path="true" style="true" version="version">
+                <Image id="_n_452" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_454" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-918 sk-Text">每日签到</View>
+              </Navigator>
+              <Navigator class="item" delta="1" extra-data="null" hover-class="Navigator-hover" hover-start-time="50" hover-stay-time="600" id="_n_459" open-type="navigate" path="true" style="true" version="version">
+                <Image id="_n_456" mode="scaleToFill" style="true" class="sk-Image"></Image>
+                <View hover-class="none" hover-start-time="50" hover-stay-time="400" id="_n_458" style="background-position-x: 50%;" class="sk-transparent sk-Text-14-2857-334 sk-Text">反馈建议</View>
+              </Navigator>
+            </View>
+          </View>
+        </View>
+      </View>
+    )
+
+  }
+
+
+}
+
+
+
+export default MineSkeleton;
+
+
+
+
+
